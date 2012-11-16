c8eb10e48e0ec9968304346e44ddd3e869a47ebf
Mark Williamson <mw529@cam.ac.uk>
2012-11-16 16:12:26 +0000

    First part of a typical protocol

--- OpenMM/Typical_MD_Protocol/TypicalProtocol.py
+++ OpenMM/Typical_MD_Protocol/TypicalProtocol.py
@@ -1,11 +1,8 @@
-#
 # Typical AMBER MD protocol 
 
 from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
-from sys import stdout
-import time
 
 
 # Reference
@@ -18,110 +15,52 @@ platform=openmm.Platform_getPlatform(2)
 
 
 
+#####################
+#####################
+## Leap like stages##
+#####################
+#####################
+
+####################################################
+# Build system from PDB and assign AMBER FF99SB FF #
+####################################################
+
+pdb = PDBFile('1UBQ.pdb')
+forceField = ForceField('amber99sb.xml', 'tip3p.xml')
+
+
+# Add missing hydrogens
+modeller = Modeller(pdb.topology, pdb.positions)
+modeller.addHydrogens(forceField)
+
+# Add TIP3P solvent
+modeller.addSolvent(forceField, model='tip3p', padding=10*angstrom)
+
+
+
+print "Number of atoms %i"      % len(modeller.positions)
+
 ###################
-# 1) Minimisation #
+# Minimisation    #
 ###################
-prmtop = AmberPrmtopFile('prmtop')
-inpcrd = AmberInpcrdFile('inpcrd',  loadBoxVectors=True)
 
-print "Number of atoms %i"      % len(inpcrd.positions)
+system = forceField.createSystem(modeller.topology, nonbondedMethod=PME)
 
-integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 2*femtoseconds)
-system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=8*angstrom )
+integrator = VerletIntegrator(1*femtosecond)
+simulation = Simulation(modeller.topology, system, integrator, platform)
+simulation.context.setPositions(modeller.positions)
+simulation.minimizeEnergy(maxIterations=100)
 
-simulation = Simulation(prmtop.topology, system, integrator, platform)
+# Saving minimised positions
+positions = simulation.context.getState(getPositions=True).getPositions()
+PDBFile.writeFile(simulation.topology, positions, open('output.pdb', 'w'))
 
-print "Platform: %s" % (simulation.context.getPlatform().getName())
-simulation.context.setPositions(inpcrd.positions)
-#simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True)
+#######################
+#######################
+## PMEMD like stages ##
+#######################
+#######################
 
-
-simulation.reporters.append(PDBReporter('min.pdb', 1))
-# Minimise system with L-BFGS algorithm
-LocalEnergyMinimizer.minimize(simulation.context,)
-
-# Save context?
-#for x in state.getPostions().value_in_unit(angstrom):
-#  print x 
-
-##############
-# 2) Heating #
-##############
-#prmtop = AmberPrmtopFile('prmtop')
-#inpcrd = AmberInpcrdFile('inpcrd.minized',  loadVelocities=True, loadBoxVectors=True)
-
-#print "Number of atoms %i"      % len(inpcrd.positions)
-#print "Number of velocities %i" % len(inpcrd.velocities)
-
-#integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 2*femtoseconds)
-#simulation = Simulation(prmtop.topology, system, integrator, platform)
-
-#simulation.context.setPositions(inpcrd.positions)
-#simulation.context.setVelocities(inpcrd.velocities)
-
-#print "Platform: %s" % (simulation.context.getPlatform().getName())
-
-#simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True, density=True))
-#simulation.reporters.append(PDBReporter('heating.pdb', 1000))
-
-#simulation.step(35000) # i.e. 20,000 fs == 20 ps == 0.02 ns
-
-
-#Save context?
-
-
-##########################
-# NTP Density correction #
-##########################
-
-#prmtop = AmberPrmtopFile('prmtop')
-#inpcrd = AmberInpcrdFile('inpcrd.minized',  loadVelocities=True, loadBoxVectors=True)
-
-#print "Number of atoms %i"      % len(inpcrd.positions)
-#print "Number of velocities %i" % len(inpcrd.velocities)
-
-#integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 2*femtoseconds)
-# Barostat
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Refs
-# Python API docs
-# https://simtk.org/api_docs/openmm/api4_1/python/main.html
-
-
-
-# http://wiki.simtk.org/openmm/BenchmarkOpenMMDHRF
-#  1xC2070 = 30.9 ns/day
-#
-# http://ambermd.org/gpus/benchmarks.htm
-#  1xM2090 = 43.74 ns/day
-#  
-
-
-
-
-# Results
-# AMBER11 ecc on 			38.12 ns/day  (45.28s run time)
-# AMBER11 ecc off 			42.48 ns/day  (40.67s run time)
-
-# OpenMM 4.1.1/OpenCL ecc on 		17.38 ns/day  (99.41)
-# OpenMM 4.1.1/OpenCL ecc off 		18.40 ns/day  (93.91 run time)
-
-# OpenMM 4.1.1/CUDA ecc off		11.77 ns/day (146.69 run time)
-
+####################
+# Thermalisation   #
+####################
