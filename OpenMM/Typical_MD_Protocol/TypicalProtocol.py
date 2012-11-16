d3f36e540037efa2b59f6e826b63f90aaca1c0d8
Mark Williamson <mw529@cam.ac.uk>
2012-11-16 18:03:06 +0000

    Update protocol

--- OpenMM/Typical_MD_Protocol/TypicalProtocol.py
+++ OpenMM/Typical_MD_Protocol/TypicalProtocol.py
@@ -4,6 +4,8 @@ from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
 
+from sys import stdout
+
 
 # Reference
 #platform=openmm.Platform_getPlatform(0)
@@ -21,9 +23,10 @@ platform=openmm.Platform_getPlatform(2)
 #####################
 #####################
 
-####################################################
-# Build system from PDB and assign AMBER FF99SB FF #
-####################################################
+#######################################################
+# 1) Build system from PDB and assign AMBER FF99SB FF #
+#######################################################
+print "Building system"
 
 pdb = PDBFile('1UBQ.pdb')
 forceField = ForceField('amber99sb.xml', 'tip3p.xml')
@@ -40,20 +43,24 @@ modeller.addSolvent(forceField, model='tip3p', padding=10*angstrom)
 
 print "Number of atoms %i"      % len(modeller.positions)
 
-###################
-# Minimisation    #
-###################
+######################
+# 2) Minimisation    #
+######################
+print "Minimising system"
 
-system = forceField.createSystem(modeller.topology, nonbondedMethod=PME)
+system = forceField.createSystem(modeller.topology, nonbondedMethod=PME, nonbondedCutoff=8*angstrom)
 
 integrator = VerletIntegrator(1*femtosecond)
 simulation = Simulation(modeller.topology, system, integrator, platform)
+
+print "Platform: %s" % (simulation.context.getPlatform().getName())
+
 simulation.context.setPositions(modeller.positions)
-simulation.minimizeEnergy(maxIterations=100)
+simulation.minimizeEnergy(maxIterations=1000)
 
 # Saving minimised positions
 positions = simulation.context.getState(getPositions=True).getPositions()
-PDBFile.writeFile(simulation.topology, positions, open('output.pdb', 'w'))
+PDBFile.writeFile(simulation.topology, positions, open('minisation.pdb', 'w'))
 
 #######################
 #######################
@@ -61,6 +68,77 @@ PDBFile.writeFile(simulation.topology, positions, open('output.pdb', 'w'))
 #######################
 #######################
 
-####################
-# Thermalisation   #
-####################
+################################
+# 3) Thermalisation under NVT  #
+################################
+print "Heating system under NVT"
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 2*femtoseconds)
+
+# Note, new system
+system = forceField.createSystem(modeller.topology, nonbondedMethod=PME, nonbondedCutoff=8*angstrom, constraints=HBonds )
+
+simulation = Simulation(modeller.topology, system, integrator, platform)
+simulation.context.setPositions(positions)
+
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True, density=True))
+simulation.reporters.append(PDBReporter('heating.pdb', 1000))
+
+simulation.step(35000) # i.e. 20,000 fs == 20 ps == 0.02 ns
+
+# Save the positions and velocities
+positions = simulation.context.getState(getPositions=True).getPositions()
+velocities = simulation.context.getState(getVelocities=True).getVelocities()
+
+
+
+####################################
+# 4) Density correction under NPT  #
+####################################
+print "Density correction under NPT"
+
+system.addForce(MonteCarloBarostat(1*bar, 300*kelvin))
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 2*femtoseconds)
+
+simulation = Simulation(modeller.topology, system, integrator, platform)
+
+simulation.context.setPositions(positions)
+simulation.context.setVelocities(velocities)
+
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True, density=True))
+simulation.reporters.append(PDBReporter('density.pdb', 1000))
+
+simulation.step(35000) # i.e. 20,000 fs == 20 ps == 0.02 ns
+
+# Save the positions and velocities
+positions = simulation.context.getState(getPositions=True).getPositions()
+velocities = simulation.context.getState(getVelocities=True).getVelocities()
+
+
+####################################
+# 5) Production under NPT          #
+####################################
+print "Production under NPT"
+
+simulation.context.setPositions(positions)
+simulation.context.setVelocities(velocities)
+
+
+# Report every 0.1 ns / 100 ps
+simulation.reporters.append(StateDataReporter(stdout, 50000, step=True, potentialEnergy=True, temperature=True, density=True))
+simulation.reporters.append(PDBReporter('production.pdb', 50000))
+
+# 10 ns
+simulation.step(5000000) 
+
+# Save the positions and velocities
+positions = simulation.context.getState(getPositions=True).getPositions()
+velocities = simulation.context.getState(getVelocities=True).getVelocities()
+
+
+
+
+
+
+
+
+
