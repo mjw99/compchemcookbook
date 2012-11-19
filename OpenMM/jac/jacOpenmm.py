0190d58f09452524b7d09acfd9317bab819b6f74
Mark Williamson <mw529@cam.ac.uk>
2012-11-19 17:15:37 +0000

    Update benchmarks

--- OpenMM/jac/jacOpenmm.py
+++ OpenMM/jac/jacOpenmm.py
@@ -7,14 +7,17 @@ from sys import stdout
 import time
 
 
-# Reference
-#platform=openmm.Platform_getPlatform(0)
+# Run on multiple cards
+# 0  Tesla M2090
+# 1  Tesla C2075
+# 2  Tesla C2075
+#platformProperties = {"OpenCLDeviceIndex":"0"}
+#platformProperties = {"OpenCLDeviceIndex":"1"}
+platformProperties = {"OpenCLDeviceIndex":"0,1,2"}
+#platformProperties = {"OpenCLDeviceIndex":"1,2"}
+platform = openmm.Platform_getPlatformByName("OpenCL")
+print "Speed relative to reference is : " + str(platform.getSpeed())
 
-# CUDA
-#platform=openmm.Platform_getPlatform(1)
-
-# OpenCL
-platform=openmm.Platform_getPlatform(2)
 
 
 
@@ -22,10 +25,10 @@ platform=openmm.Platform_getPlatform(2)
 prmtop = AmberPrmtopFile('prmtop7')
 inpcrd = AmberInpcrdFile('inpcrd.equil.openmm',  loadVelocities=True, loadBoxVectors=True)
 
-system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.8*nanometer, constraints=HBonds )
+system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.8*nanometer, constraints=HBonds)
 integrator = VerletIntegrator(2*femtoseconds)
 
-simulation = Simulation(prmtop.topology, system, integrator, platform)
+simulation = Simulation(prmtop.topology, system, integrator, platform, platformProperties)
 print "Platform: %s" % (simulation.context.getPlatform().getName())
 
 print "Number of atoms %i"      % len(inpcrd.positions)
@@ -71,11 +74,16 @@ print str(NsPerDay)  + " nS/day"
 
 
 # Results
-# AMBER11 ecc on 			38.12 ns/day  (45.28s run time)
-# AMBER11 ecc off 			42.48 ns/day  (40.67s run time)
+# AMBER11, M2090,  ecc on 			38.12 ns/day  (45.28s run time)
+# AMBER11, M2090,  ecc off 			42.48 ns/day  (40.67s run time)
 
-# OpenMM 4.1.1/OpenCL ecc on 		17.38 ns/day  (99.41)
-# OpenMM 4.1.1/OpenCL ecc off 		18.40 ns/day  (93.91 run time)
 
-# OpenMM 4.1.1/CUDA ecc off		11.77 ns/day (146.69 run time)
+# OpenMM 4.1.1/OpenCL, M2090,  ecc on 		17.38 ns/day  (99.41 run time)
+# OpenMM 4.1.1/OpenCL, M2090,  ecc off 		18.40 ns/day  (93.91 run time)
+
+# OpenMM 4.1.1/OpenCL, C2075,  ecc on 		12.31 ns/day  (140.32 run time)
+# OpenMM 4.1.1/OpenCL, 2xC2075,  ecc on 	19.24 ns/day  (89.81 run time)
+# OpenMM 4.1.1/OpenCL, 1XM2090+2xC2075, ecc on	25.55 ns/day  (67.60 run time)
+
+# OpenMM 4.1.1/CUDA, M2090,  ecc off		11.77 ns/day (146.69 run time)
 
