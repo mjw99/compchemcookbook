f151df1484749104e384f5bdee6f016e29479304
Mark Williamson <mw529@cam.ac.uk>
2012-11-21 17:15:18 +0000

    Lambda mapping is now working

--- OpenMM/lamba_scaling_example/two_atom.py
+++ OpenMM/lamba_scaling_example/two_atom.py
@@ -13,20 +13,12 @@ from array import *
 # Specify simulation parameters
 # =============================================================================
 
-nparticles = 216 # number of particles
+nparticles = 2 # number of particles
 
 mass = 39.9 * amu # mass
 sigma = 3.4 * angstrom # Lennard-Jones sigma
 epsilon = 0.238 * kilocalories_per_mole # Lennard-Jones well-depth
-charge = 0.0 * elementary_charge # argon model has no charge
 
-nequil_steps = 5000 # number of dynamics steps for equilibration
-nprod_steps = 50000 # number of dynamics steps per production iteration
-
-reduced_density = 0.85 # reduced density rho*sigma^3
-temperature = 300 * kelvin # temperature
-pressure = 1 * atmosphere # pressure
-collision_rate = 5.0 / picosecond # collision rate for Langevin thermostat
 timestep = 1 * femtosecond # integrator timestep
 
 
@@ -34,9 +26,8 @@ timestep = 1 * femtosecond # integrator timestep
 # Compute box size.
 # =============================================================================
 
-volume = nparticles*(sigma**3)/reduced_density
-box_edge = volume**(1.0/3.0)
-cutoff = min(box_edge*0.49, 2.5*sigma) # Compute cutoff
+box_edge = 100 * angstrom
+cutoff = 8 * angstrom
 print "sigma = %s" % sigma
 print "box_edge = %s" % box_edge
 print "cutoff = %s" % cutoff
@@ -46,8 +37,7 @@ print "cutoff = %s" % cutoff
 # =============================================================================
 
 # Create argon system where first particle is alchemically modified by lambda_value.
-# It is currently zero
-lambda_value = 0.0
+lambda_value = 1.0
 
 
 system = System()
@@ -55,12 +45,6 @@ system.setDefaultPeriodicBoxVectors(Vec3(box_edge, 0, 0), Vec3(0, box_edge, 0), 
 
 topology = Topology()
 newChain = topology.addChain()
-
-
-# Normal nonbonded
-#nonBondedForce = NonbondedForce()
-#nonBondedForce.setNonbondedMethod(NonbondedForce.CutoffPeriodic)
-#nonBondedForce.setCutoffDistance(cutoff)
 
 
 # Modified VdW
@@ -90,7 +74,6 @@ for particle_index in range(nparticles):
   else:
      # Add normal particle.
      topology.addAtom("Argo", "ar", newResidue)
-     # Is there a better way to do this?
      customNonBondedForce.addParticle( array('d',[3.4*NmPerAngstrom, 0.238*KJPerKcal, 0 ])  )
 
 system.addForce(customNonBondedForce)
@@ -99,39 +82,36 @@ print "System.getNumParticles() is %i"  % system.getNumParticles()
 print "system.getNumForces() is %i " % system.getNumForces()
 
 
-# Create random initial positions.
-import numpy.random
-positions = Quantity(numpy.random.uniform(high=box_edge/angstroms, size=[nparticles,3]), angstrom)
+# Create initial positions.
+positions = [  Vec3(0,0,0), Vec3(0,0,0.1) ]
 
 
 # Create Integrator
-integrator = LangevinIntegrator(temperature, collision_rate, timestep)
+integrator = VerletIntegrator(1*femtosecond)
 simulation = Simulation(topology, system, integrator)
 
-# Initiate from Random set of positions
+# Create initial positions.
+positions = [  Vec3(0,0,0), Vec3(0,0,0.1) ]
 simulation.context.setPositions(positions)
-
-# Minimize energy from coordinates.
-simulation.minimizeEnergy()
 
 print "simulation.system.getNumForces() is %i " % simulation.system.getNumForces()
 
-# Equilibrate.
-simulation.step(nequil_steps)
-
 # Run dynamics.
-simulation.reporters.append( PDBReporter('output.pdb', 10000) )
-simulation.reporters.append( StateDataReporter(stdout, 10000, step=True, potentialEnergy=True, temperature=True) )
-
+simulation.reporters.append( PDBReporter('output.pdb', 100) )
+simulation.reporters.append( StateDataReporter(stdout, 100, step=True, potentialEnergy=True) )
 
 
 # =============================================================================
 # Lambda map in the Argon atom
 # =============================================================================
 
+# This is NVE; kinetic energy has come from the slight perturbation of the interatomic distance
 for i in range(10):
   print i
-  simulation.context.setParameter("lambda",  i*0.1  )
+
+  #1, 0.9, 0.8
+  simulation.context.setParameter("lambda",  1 - (i*0.1)  )
+  #simulation.context.setParameter("lambda",  1 )
   print "Current lambda value is " + str(simulation.context.getParameter("lambda"))
   simulation.step(10000)
 
