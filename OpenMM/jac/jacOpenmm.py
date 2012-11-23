87f13f7004dd06e3be7a446763003bd5df975355
Mark Williamson <mw529@cam.ac.uk>
2012-11-23 15:03:59 +0000

    CPU reference added

--- OpenMM/jac/jacOpenmm.py
+++ OpenMM/jac/jacOpenmm.py
@@ -6,16 +6,20 @@ from simtk.unit import *
 from sys import stdout
 import time
 
+platform = openmm.Platform_getPlatformByName("OpenCL")
+#platform = openmm.Platform_getPlatformByName("Cuda")
+#platform = openmm.Platform_getPlatformByName("Reference")
+
+
 
 # Run on multiple cards
 # 0  Tesla M2090
 # 1  Tesla C2075
 # 2  Tesla C2075
-#platformProperties = {"OpenCLDeviceIndex":"0"}
+platformProperties = {"OpenCLDeviceIndex":"0"}
 #platformProperties = {"OpenCLDeviceIndex":"1"}
-platformProperties = {"OpenCLDeviceIndex":"0,1,2"}
+#platformProperties = {"OpenCLDeviceIndex":"0,1,2"}
 #platformProperties = {"OpenCLDeviceIndex":"1,2"}
-platform = openmm.Platform_getPlatformByName("OpenCL")
 print "Speed relative to reference is : " + str(platform.getSpeed())
 
 
@@ -26,6 +30,7 @@ prmtop = AmberPrmtopFile('prmtop7')
 inpcrd = AmberInpcrdFile('inpcrd.equil.openmm',  loadVelocities=True, loadBoxVectors=True)
 
 system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.8*nanometer, constraints=HBonds)
+# Remember, this is being run NVE
 integrator = VerletIntegrator(2*femtoseconds)
 
 simulation = Simulation(prmtop.topology, system, integrator, platform, platformProperties)
@@ -34,26 +39,26 @@ print "Platform: %s" % (simulation.context.getPlatform().getName())
 print "Number of atoms %i"      % len(inpcrd.positions)
 print "Number of velocities %i" % len(inpcrd.velocities)
 
-
 simulation.context.setPositions(inpcrd.positions)
 simulation.context.setVelocities(inpcrd.velocities)
 
 simulation.reporters.append(PDBReporter('output.pdb', 1000))
-simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,  totalEnergy=True, kineticEnergy=True, potentialEnergy=True, temperature=True))
+#simulation.reporters.append(StateDataReporter(stdout, 10, step=True,  totalEnergy=True, kineticEnergy=True, potentialEnergy=True, temperature=True))
 
 start_time = time.time()
 simulation.step(10000) # i.e. 20,000 fs == 20 ps == 0.02 ns
 
 # 100 seconds to run 0.02 ns 
-# Hence it will take 1/0.02  * 100s to run one nS.
-totalDynmaicRunTimeInSeconds = time.time() - start_time
+# Hence it will take 1/0.02  * 100s to run one ns.
+totalDynamicsRunTimeInSeconds = time.time() - start_time
 
-timeNeedToRunOneNsinSeconds = (1/0.02) * totalDynmaicRunTimeInSeconds
+timeNeedToRunOneNsinSeconds = (1/0.02) * totalDynamicsRunTimeInSeconds
 
 NsPerDay = 86400 / timeNeedToRunOneNsinSeconds
 
 
-print str(totalDynmaicRunTimeInSeconds) + " seconds"
+print str(totalDynamicsRunTimeInSeconds) + " seconds"
 print str(timeNeedToRunOneNsinSeconds) + " is the time needed to run 1 ns"
 print str(NsPerDay)  + " nS/day"
 
@@ -72,11 +77,16 @@ print str(NsPerDay)  + " nS/day"
 
 
 
+###########
+# Results #
+###########
 
-# Results
 # AMBER11, M2090,  ecc on 			38.12 ns/day  (45.28s run time)
 # AMBER11, M2090,  ecc off 			42.48 ns/day  (40.67s run time)
 
+
+
+# OpenMM 4.1.1/Reference 			0.07 ns/day  (24366.66 run time)
 
 # OpenMM 4.1.1/OpenCL, M2090,  ecc on 		17.38 ns/day  (99.41 run time)
 # OpenMM 4.1.1/OpenCL, M2090,  ecc off 		18.40 ns/day  (93.91 run time)
@@ -86,4 +96,6 @@ print str(NsPerDay)  + " nS/day"
 # OpenMM 4.1.1/OpenCL, 1XM2090+2xC2075, ecc on	25.55 ns/day  (67.60 run time)
 
 # OpenMM 4.1.1/CUDA, M2090,  ecc off		11.77 ns/day (146.69 run time)
+
+
 
