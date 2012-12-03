64a8639c0e6b0d42d65c4047e1e67de59a09d2e3
Mark Williamson <mw529@cam.ac.uk>
2012-12-03 17:09:25 +0000

    Example demonstrating AMBER's incorrect value for the vacuum permittivity value

---
+++ OpenMM/AMBER_EE_differences/AMBER_EPSILON_ZERO.py
@@ -0,0 +1,85 @@
+# AMBER optimises its electrostatic calculation between pairs (EE) by 
+# incorporating constants into the charges values within a prmtop file
+# See: http://ambermd.org/Questions/units.html
+# The charge values are multipled by 18.2223 and this 
+# is defined as AMBER_ELECTROSTATIC in SANDER
+
+# However, repeating the EE in other engines, one sees a slight 
+# difference against SANDER and PMEMD.
+
+
+# For example, with the ACE system:
+# EE OpenMM 4.4.1 Reference -27.8352952389 kcal/mol
+# EE JAMBER                 -27.8342137002 kcal/mol
+#                                 ^^^^^^^^^^^^^^^^
+
+
+
+# The origin of this lies with the fact that EPSILON_ZERO used by AMBER
+# to calculate this is slightly wrong. This script carries out the back 
+# calculation to work out this difference:
+
+#  ATKINS's molecular Quantum Mechanics  8.854187817e-12
+#  AMBER                                 8.85449607758e-12
+#                                             ^^^^^^^^^^^
+
+# Hence AMBER is wrong in the 4th decimal place.
+# CHARMM suffers from something similar...
+
+
+
+
+
+# AMBER_ELECTROSTATIC
+AMBER_CHARGE_PREFACTOR =  18.2223 
+# This is  AMBER_CHARGE_PREFACTOR**2
+AMBER_PREFACTOR = 332.0522173
+
+KCAL_TO_JOULE = 4184 
+JOULES_TO_KILOJOULE = 0.001
+
+
+## OpenMM
+PI = 3.14159265358979323846
+E_CHARGE = 1.60217733e-19
+AVOGADRO = 6.0221367e23
+
+
+## ATKINS's molecular Quantum Mechanics
+#E_CHARGE     = 1.602176e-19
+EPSILON_ZERO = 8.854187817e-12
+#AVOGADRO     = 6.02214e23
+
+## SANDER Constants
+#PI = 3.14159265358979323846
+#E_CHARGE = 1.60217733e-19
+#AVOGADRO = 6.0221367e23
+
+AMBER_EPSILON_ZERO = 8.85449607758e-12
+
+
+
+
+print "AMBER's prefactor " + str(AMBER_PREFACTOR)
+# Exact
+# 332.063408432
+print  "'Exact' prefactor " + str((( ( 1 * E_CHARGE  * E_CHARGE) / ( 4 * PI * EPSILON_ZERO * 1E-10 ) ) * AVOGADRO) / KCAL_TO_JOULE)
+
+
+
+
+# OpenMM's 
+# ./OpenMM4.1.1-Source/platforms/reference/src/SimTKUtilities/SimTKOpenMMRealType.h
+# ONE_4PI_EPS0 138.935456
+print "OpenMM's ONE_4PI_EPS0 is " + str(138.935456)
+
+print "OpenMM's ONE_4PI_EPS0 should be " +   str((( ( 1 * E_CHARGE  * E_CHARGE) / ( 4 * PI * EPSILON_ZERO * 1E-9 ) ) * AVOGADRO) * JOULES_TO_KILOJOULE)
+
+
+# 332.0522173
+# What is AMBER Using for EPSILON_ZERO?
+print "Back calculating AMBER_EPSILON_ZERO " + str((( ( 1 * E_CHARGE  * E_CHARGE) / ( 4 * PI * AMBER_PREFACTOR * 1E-10 ) ) * AVOGADRO) / KCAL_TO_JOULE)
+
+
+# Hence, what would OpenMM's ONE_4PI_EPS0's be, using AMBER_EPSILON_ZERO ?
+print "Broken ONE_4PI_EPS0 would be " + str( (( ( 1 * E_CHARGE  * E_CHARGE) / ( 4 * PI * AMBER_EPSILON_ZERO * 1E-9 ) ) * AVOGADRO) * JOULES_TO_KILOJOULE)
