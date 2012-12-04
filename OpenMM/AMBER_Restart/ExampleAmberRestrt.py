1270bc5877de4aca88ab64040a42f2410c60c484
Mark Williamson <mw529@cam.ac.uk>
2012-12-04 15:01:54 +0000

    Clean up an example

--- OpenMM/AMBER_Restart/ExampleAmberRestrt.py
+++ OpenMM/AMBER_Restart/ExampleAmberRestrt.py
@@ -1,4 +1,5 @@
-# DHRF Benchmark using OpenMM
+# An example use of saving a state with MJW's
+# AmberRestrtFile object
 
 from simtk.openmm.app import *
 from simtk.openmm import *
@@ -7,44 +8,59 @@ from sys import stdout
 import time
 from AmberRestrtFile import AmberRestrtFile
 
+
+## Select which platform to use:
 #platform = openmm.Platform_getPlatformByName("OpenCL")
 #platform = openmm.Platform_getPlatformByName("Cuda")
 platform = openmm.Platform_getPlatformByName("Reference")
 
 
 
+
+## Select which card(s) to run on if using OpenCL
 # Run on multiple cards
 # 0  Tesla M2090
 # 1  Tesla C2075
 # 2  Tesla C2075
+
 platformProperties = {"OpenCLDeviceIndex":"0"}
 #platformProperties = {"OpenCLDeviceIndex":"1"}
 #platformProperties = {"OpenCLDeviceIndex":"0,1,2"}
 #platformProperties = {"OpenCLDeviceIndex":"1,2"}
-print "Speed relative to reference is : " + str(platform.getSpeed())
 
 
 
+# Load the AMBER topology and coordinate files
+# Also, read the velocities from the inpcrd file as well
 prmtop = AmberPrmtopFile('prmtop')
 inpcrd = AmberInpcrdFile('inpcrd', loadVelocities=True)
 
+
+# Typical PBC system with PME
 #system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.8*nanometer, constraints=HBonds)
+
+# Explicit solvent model
 #system = prmtop.createSystem(nonbondedMethod=NoCutoff, implicitSolvent=OBC2)
+
+# No PBC, infinite cut off
 system = prmtop.createSystem(nonbondedMethod=NoCutoff)
+
 # Remember, this is being run NVE
 integrator = VerletIntegrator(1*femtoseconds)
 
 simulation = Simulation(prmtop.topology, system, integrator, platform, platformProperties)
+
 print "Platform: %s" % (simulation.context.getPlatform().getName())
 
 print "Number of atoms %i"      % len(inpcrd.positions)
+
+# Set the positions and velocities from the AMBER files
 simulation.context.setPositions(inpcrd.positions)
 simulation.context.setVelocities(inpcrd.velocities)
 
-simulation.reporters.append(PDBReporter('output.pdb', 1))
-simulation.reporters.append(StateDataReporter(stdout, 1, step=True,  totalEnergy=True, kineticEnergy=True, potentialEnergy=True, temperature=True))
 
-
+# We will skip this since, I want to check that I get the same
+# result as I read in
 #simulation.step(1)
 
 # Write the AMBER restart
