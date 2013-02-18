804aca9f1024effab909dfda0544cf638e356f20
Mark Williamson <mw529@cam.ac.uk>
2013-02-18 13:42:46 +0000

    Clean up energy decomp example

--- OpenMM/Energy_decomposition/PotentialEnergyDecomp.py
+++ OpenMM/Energy_decomposition/PotentialEnergyDecomp.py
@@ -9,34 +9,16 @@ from sys import stdout
 import time
 from copy import deepcopy 
 
-#platform = openmm.Platform_getPlatformByName("OpenCL")
-#platform = openmm.Platform_getPlatformByName("Cuda")
 platform = openmm.Platform_getPlatformByName("Reference")
-
-
-
-# Run on multiple cards
-# 0  Tesla M2090
-# 1  Tesla C2075
-# 2  Tesla C2075
-platformProperties = {"OpenCLDeviceIndex":"0"}
-#platformProperties = {"OpenCLDeviceIndex":"1"}
-#platformProperties = {"OpenCLDeviceIndex":"0,1,2"}
-#platformProperties = {"OpenCLDeviceIndex":"1,2"}
-
 
 prmtop = AmberPrmtopFile('prmtop')
 inpcrd = AmberInpcrdFile('inpcrd')
 
-#system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.8*nanometer, constraints=HBonds)
-#system = prmtop.createSystem(nonbondedMethod=NoCutoff, implicitSolvent=OBC2)
-#system = prmtop.createSystem(nonbondedMethod=NoCutoff, removeCMMotion=False)
 system = prmtop.createSystem(nonbondedMethod=NoCutoff)
 
-# Remember, this is being run NVE
 integrator = VerletIntegrator(1*femtoseconds)
 
-simulation = Simulation(prmtop.topology, system, integrator, platform, platformProperties)
+simulation = Simulation(prmtop.topology, system, integrator, platform)
 print "Platform: %s" % (simulation.context.getPlatform().getName())
 print "Number of atoms %i"      % len(inpcrd.positions)
 
