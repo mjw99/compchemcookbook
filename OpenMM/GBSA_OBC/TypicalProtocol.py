7c7f5dc73af5623b2111681e5936626781bc7f8c
Mark Williamson <mw529@cam.ac.uk>
2013-07-01 17:36:08 +0100

    Use nonbondedMethod=CutoffNonPeriodic so that PME is not used. Clean up comments

--- OpenMM/GBSA_OBC/TypicalProtocol.py
+++ OpenMM/GBSA_OBC/TypicalProtocol.py
@@ -1,5 +1,4 @@
-# Typical AMBER MD protocol 
-
+# Typical AMBER GB MD protocol with N-acetyl-alanine-N'-methylamide
 from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
@@ -35,7 +34,7 @@ PDBFile.writeFile(modeller.getTopology(), modeller.getPositions(),open('modeller
 ######################
 print "Minimising system"
 
-system = forceField.createSystem(modeller.topology, nonbondedCutoff=20*angstrom)
+system = forceField.createSystem(modeller.topology, nonbondedCutoff=20*angstrom, nonbondedMethod=CutoffNonPeriodic)
 
 f = open('system.xml','w')
 f.write(XmlSerializer.serializeSystem(system))
@@ -58,18 +57,20 @@ PDBFile.writeFile(simulation.topology, positions, open('minimisation.pdb', 'w'))
 #######################
 #######################
 
-################################
-# 3) Thermalisation under NVT  #
-################################
+#####################
+# 3) Thermalisation #
+#####################
 print "Heating system"
 integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 1*femtoseconds)
 
 # Note, new system
-system = forceField.createSystem(modeller.topology, nonbondedCutoff=20*angstrom)
+system = forceField.createSystem(modeller.topology, nonbondedCutoff=20*angstrom, nonbondedMethod=CutoffNonPeriodic)
 
 # The reaction field method is turned on for NonbondedForce objects even when 
-# GBSAOBCForce in present in a system. This is wrong and causes problems: see
-# https://github.com/SimTk/openmm/issues/26 . This turns the reaction field off.
+# GBSAOBCForce in present in a system. This is wrong and causes problems
+#  https://github.com/SimTk/openmm/issues/26 
+#  https://simtk.org/tracker/index.php?func=detail&aid=1881&group_id=161&atid=435
+# This turns the reaction field off.
 for i in range(system.getNumForces()):
    print type(system.getForce(i))
    if (type(system.getForce(i)) == openmm.NonbondedForce):
@@ -88,10 +89,8 @@ simulation.step(35000)
 positions = simulation.context.getState(getPositions=True).getPositions()
 velocities = simulation.context.getState(getVelocities=True).getVelocities()
 
-
 #clear reporters
 simulation.reporters = []
-
 
 # Save the positions and velocities
 positions = simulation.context.getState(getPositions=True).getPositions()
@@ -101,27 +100,17 @@ velocities = simulation.context.getState(getVelocities=True).getVelocities()
 simulation.reporters = []
 
 
-####################################
-# 5) Production under NPT          #
-####################################
+##################
+# 4) Production  #
+##################
 print "Production"
 
 simulation.context.setPositions(positions)
 simulation.context.setVelocities(velocities)
 
-
-# Report every  2 ps
+# Report every 0.1 ps
 simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, time=True, potentialEnergy=True, temperature=True) )
 simulation.reporters.append(PDBReporter('production.pdb', 1000))
 
 # 1 ns
-simulation.step(500000) 
-
-# Save the positions and velocities
-positions = simulation.context.getState(getPositions=True).getPositions()
-velocities = simulation.context.getState(getVelocities=True).getVelocities()
-
-
-
-
-
+simulation.step(1000000) 
