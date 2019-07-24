6576f9541c70a963645b487ebcb8b74175b7fee8
Mark Williamson <mw529@cam.ac.uk>
2019-07-24 11:28:05 +0100

    Python 3 fixes

--- OpenMM/Energy_decomposition/PotentialEnergyDecomp.py
+++ OpenMM/Energy_decomposition/PotentialEnergyDecomp.py
@@ -15,31 +15,31 @@ integrator = VerletIntegrator(1*femtoseconds)
 simulation = Simulation(prmtop.topology, system, integrator, platform)
 simulation.context.setPositions(inpcrd.positions)
 
-print "Platform: %s" % (simulation.context.getPlatform().getName())
-print "Number of atoms %i"      % len(inpcrd.positions)
+print("Platform: ", (simulation.context.getPlatform().getName()))
+print("Number of atoms ", len(inpcrd.positions))
 
-print ""
+print("")
 # Entire system
 state = simulation.context.getState( getEnergy=True)
-print "Total potential energy is " +  str(state.getPotentialEnergy().in_units_of(kilocalorie/mole))
-print ""
+print("Total potential energy is ", str(state.getPotentialEnergy().in_units_of(kilocalorie/mole)))
+print("")
 
 for i in range(system.getNumForces()):
     system.getForce(i).setForceGroup(i)
 
 for i in range(system.getNumForces()):
-    print type(system.getForce(i))
+    print(type(system.getForce(i)))
     state = simulation.context.getState(getEnergy=True, groups=1<<i)
-    print state.getPotentialEnergy().in_units_of(kilocalorie/mole)
+    print(state.getPotentialEnergy().in_units_of(kilocalorie/mole))
 
 
-print ""
+print("")
 # Harmonic Bond
-print str(5.6366273691689504) + " kcal/mol (JAMBER)"
+print( str(5.6366273691689504), " kcal/mol (JAMBER)")
 # Harmonic Angle
-print str(8.2046173826501736) + " kcal/mol (JAMBER)"
+print( str(8.2046173826501736), " kcal/mol (JAMBER)")
 # Torsions
-print str(11.365686464834990) + " kcal/mol (JAMBER)"
+print( str(11.365686464834990), " kcal/mol (JAMBER)")
 # NB
 JAMBER_Result = -79.994516268914055 + -1.0455001424914672 + 4.4614567778860472 + 48.744345933282325
-print str( JAMBER_Result  ) + " kcal/mol (JAMBER)"
+print(str( JAMBER_Result  ) , " kcal/mol (JAMBER)")
