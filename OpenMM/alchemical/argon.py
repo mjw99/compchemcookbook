d17eb3089170df960ca62458905f89174f4e9a38
Mark Williamson <mjw@mjw.name>
2024-12-02 14:02:03 +0000

    Update with a modern version

--- OpenMM/alchemical/argon.py
+++ OpenMM/alchemical/argon.py
@@ -1,103 +1,123 @@
-# conda install -c omnia openmmtools
-# conda install -c omnia pymbar
-# 
-
-from simtk import openmm, unit
-
-# Create a Lennard-Jones fluid
-pressure = 80*unit.atmospheres
-temperature = 120*unit.kelvin
-collision_rate = 5/unit.picoseconds
-timestep = 2.5*unit.femtoseconds
-
-
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
 from openmmtools.testsystems import LennardJonesFluid
-sigma = 3.4*unit.angstrom
-epsilon = 0.238 * unit.kilocalories_per_mole
+
+
+# Simulation settings
+pressure = 80*atmospheres
+temperature = 120*kelvin
+collision_rate = 5/picoseconds
+timestep = 2.5*femtoseconds
+
+
+# Create a Lennard Jones test fluid
+sigma = 3.4*angstrom
+epsilon = 0.238 * kilocalories_per_mole
 fluid = LennardJonesFluid(sigma=sigma, epsilon=epsilon)
-[system, positions] = [fluid.system, fluid.positions]
+[topology, system, positions] = [fluid.topology, fluid.system, fluid.positions]
 
 # Add a barostat
-barostat = openmm.MonteCarloBarostat(pressure, temperature)
+barostat = MonteCarloBarostat(pressure, temperature)
 system.addForce(barostat)
-
 
 # Retrieve the NonbondedForce
 forces = { force.__class__.__name__ : force for force in system.getForces() }
 nbforce = forces['NonbondedForce']
 
 # Add a CustomNonbondedForce to handle only alchemically-modified interactions
+
+# Make two sets of particles, one that contains just the particle we will alchemically annihilate
+# and the other which contains all the other particles.
 alchemical_particles = set([0])
 chemical_particles = set(range(system.getNumParticles())) - alchemical_particles
 
+
+# Define the energy function for the CustomNonbondedForce
+# when lambda is 1.0 it is a normal LJ potential, when lambda is 0.0 the interaction vanishes 
 energy_function = 'lambda*4*epsilon*x*(x-1.0); x = (sigma/reff_sterics)^6;'
 energy_function += 'reff_sterics = sigma*(0.5*(1.0-lambda) + (r/sigma)^6)^(1/6);'
 energy_function += 'sigma = 0.5*(sigma1+sigma2); epsilon = sqrt(epsilon1*epsilon2);'
+custom_force = CustomNonbondedForce(energy_function)
 
-custom_force = openmm.CustomNonbondedForce(energy_function)
+# Set the custom force to use a cutoff
+custom_force.setNonbondedMethod(CustomNonbondedForce.CutoffPeriodic)
+# and set the cutoff distance to be the same as the existing nbforce
+custom_force.setCutoffDistance(nbforce.getCutoffDistance())
+
+# Add lambda as a parameter we can change during the simulation
 custom_force.addGlobalParameter('lambda', 1.0)
+
+# set the values of sigma and epsilon by copying them from the existing NonBondedForce
 custom_force.addPerParticleParameter('sigma')
 custom_force.addPerParticleParameter('epsilon')
-
 for index in range(system.getNumParticles()):
     [charge, sigma, epsilon] = nbforce.getParticleParameters(index)
     custom_force.addParticle([sigma, epsilon])
     if index in alchemical_particles:
+        # remove the alchemical particle from the existing NonBondedForce
         nbforce.setParticleParameters(index, charge*0, sigma, epsilon*0)
+
+# Set the custom force to occur between just the alchemical particle and the other particles
 custom_force.addInteractionGroup(alchemical_particles, chemical_particles)
 system.addForce(custom_force)
 
+# Create an integrator
+integrator = LangevinIntegrator(temperature, collision_rate, timestep)
 
-# Create a context
-integrator = openmm.LangevinIntegrator(temperature, collision_rate, timestep)
-context = openmm.Context(system, integrator)
-context.setPositions(positions)
+# Create a simulation
+simulation = Simulation(topology, system, integrator)
+simulation.context.setPositions(positions)
 
 # Minimize energy
 print('Minimizing energy...')
-openmm.LocalEnergyMinimizer.minimize(context)
+#LocalEnergyMinimizer.minimize(context)
+simulation.minimizeEnergy()
+
 
 # Collect data
-nsteps = 2500 # number of steps per sample
-niterations = 50 # number of samples to collect per alchemical state
 
+# number of steps per sample
+nsteps = 2500
+
+# number of samples to collect per alchemical state
+niterations = 500
 
 import numpy as np
-#lambdas = np.linspace(1.0, 0.0, 10) # alchemical lambda schedule
-lambdas = np.linspace(1.0, 0.0, 32) # alchemical lambda schedule
+lambdas = np.linspace(1.0, 0.0, 10) # alchemical lambda schedule
 nstates = len(lambdas)
 u_kln = np.zeros([nstates,nstates,niterations], np.float64)
-kT = unit.AVOGADRO_CONSTANT_NA * unit.BOLTZMANN_CONSTANT_kB * integrator.getTemperature()
+kT = AVOGADRO_CONSTANT_NA * BOLTZMANN_CONSTANT_kB * integrator.getTemperature()
 for k in range(nstates):
     for iteration in range(niterations):
         print('state %5d iteration %5d / %5d' % (k, iteration, niterations))
         # Set alchemical state
-        context.setParameter('lambda', lambdas[k])
+        simulation.context.setParameter('lambda', lambdas[k])
         # Run some dynamics
-        integrator.step(nsteps)
+        simulation.step(nsteps)
         # Compute energies at all alchemical states
         for l in range(nstates):
-            context.setParameter('lambda', lambdas[l])
-            u_kln[k,l,iteration] = context.getState(getEnergy=True).getPotentialEnergy() / kT
+            simulation.context.setParameter('lambda', lambdas[l])
+            u_kln[k,l,iteration] = simulation.context.getState(getEnergy=True).getPotentialEnergy() / kT
+
 
 # Estimate free energy of Lennard-Jones particle insertion
 from pymbar import MBAR, timeseries
 
-# Subsample data to extract uncorrelated equilibrium timeseries
+## Subsample data to extract uncorrelated equilibrium timeseries
 N_k = np.zeros([nstates], np.int32) # number of uncorrelated samples
 for k in range(nstates):
-    [nequil, g, Neff_max] = timeseries.detectEquilibration(u_kln[k,k,:])
-    indices = timeseries.subsampleCorrelatedData(u_kln[k,k,:], g=g)
+    [nequil, g, Neff_max] = timeseries.detect_equilibration(u_kln[k,k,:])
+    indices = timeseries.subsample_correlated_data(u_kln[k,k,:], g=g)
     N_k[k] = len(indices)
     u_kln[k,:,0:N_k[k]] = u_kln[k,:,indices].T
 
-
-# Compute free energy differences and statistical uncertainties
+# Compute free energy differences
 mbar = MBAR(u_kln, N_k)
-[DeltaF_ij, dDeltaF_ij, Theta_ij] = mbar.getFreeEnergyDifferences()
 
-#Deltaf_ij[i,j] is the dimensionless free energy difference f_j - f_i
-print(DeltaF_ij)
+# If this fails try setting compute_uncertainty to false
+# See this issue: https://github.com/choderalab/pymbar/issues/419
+results = mbar.compute_free_energy_differences(compute_uncertainty=True)
 
-#Deltaf_ij[i,j] is the dimensionless free energy difference f_j - f_i
-print(dDeltaF_ij)
+print("Free energy change to insert a particle = ", results['Delta_f'][nstates-1,0])
+print("Statistical uncertainty = ", results['dDelta_f'][nstates-1,0])
